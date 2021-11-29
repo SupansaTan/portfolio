69d8dfc8eb21d02b5437cbac9deb617ea5d6eec3
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-11-30 00:51:14 +0700

    install bootstrap and popperjs

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>test</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component'
+
+export default class Navbar extends Vue {}
+</script>
+
+<style lang="scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Navbar/>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
 
 @Options({
   components: {
-    HelloWorld
+    Navbar
   }
 })
 export default class Home extends Vue {}
