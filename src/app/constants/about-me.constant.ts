dfde7b1d77ec072dff6f47619d9a4161a2c954a6
Supansa Tantulset <supansa.tantulset@gmail.com>
2023-05-23 21:22:29 +0700

    update about me content alignment

--- src/app/constants/about-me.constant.ts
+++ src/app/constants/about-me.constant.ts
@@ -1,11 +1,7 @@
 export const AboutMe: string =
-`Now, I have just completed my Bachelor’s degree in Computer Engineering
-at King Mongkut University of Technology North Bangkok (KMUTNB).
+`Now, I have just completed my Bachelor’s degree in Computer Engineering at King Mongkut University of Technology North Bangkok (KMUTNB).
 
-I have had the opportunity to intern as a Full Stack Developer for 3 months,
-working mainly with Angular and ASP.NET Core frameworks.
+I have had the opportunity to intern as a Full Stack Developer for 3 months, working mainly with Angular and ASP.NET Core frameworks.
 
-I also open to doing freelance work during summer semester of second-year,
-primarily creating websites from designs. therefore, I usually work with React,
-Angular, HTML, CSS, SCSS, and JavaScript.
+I also open to doing freelance work during summer semester of second-year, primarily creating websites from designs. therefore, I usually work with React, Angular, HTML, CSS, SCSS, and JavaScript.
 `
