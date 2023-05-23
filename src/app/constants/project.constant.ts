ceb1023e56a2533c44074edb82cf6b7c5acba6e0
Supansa Tantulset <supansa.tantulset@gmail.com>
2023-05-23 17:09:02 +0700

    add project image

--- src/app/constants/project.constant.ts
+++ src/app/constants/project.constant.ts
@@ -7,7 +7,18 @@ export const ProjectList: Array<ProjectModel> = [
     description:
       `Web application for finding and reviewing restaurants, a mini-game for the user who is unsure
       what to eat and dashboard for restaurant owner to get feedback their restaurant`,
-    imagePath: [],
+    imagePath: [
+      'https://drive.google.com/uc?export=view&id=1rmzQAdNFup-eiDa_CW4HFhj1U8hxgDmB',
+      'https://drive.google.com/uc?export=view&id=1rlj9x_vO3Bq0bOc9q-Uz94AoQjnXTILV',
+      'https://drive.google.com/uc?export=view&id=12Au4rJ9OTAjHXWGU7MZUDCIuADLMpp6K',
+      'https://drive.google.com/uc?export=view&id=116yqDpAUUTfI_TYKRQHhZzLShkz9YrUZ',
+      'https://drive.google.com/uc?export=view&id=1kCUXlALOT43VOezH9PpNP_vrppMrqwxg',
+      'https://drive.google.com/uc?export=view&id=1wIJGqBiuvkRIlTPqpEzp03LlZE4n4O6j',
+      'https://drive.google.com/uc?export=view&id=14XFL8phEti12eez2kjs0iURPHBVUSvfk',
+      'https://drive.google.com/uc?export=view&id=15qqgzHbZBRkI-XXp7lIkwHknX3zfZR0k',
+      'https://drive.google.com/uc?export=view&id=12pzOxtkE8UPO8mZfof2bh3DrLc6q5sP2',
+      'https://drive.google.com/uc?export=view&id=1J6xvv6LWl2dwWnJr5wb6_euu7xFdAuzw',
+    ],
     skills: [
       'Angular', 'ASP.NET Core', 'C#', 'TypeScript', 'SCSS', 'Cypress', 'Cucumber',
       'xUnit.net', 'AWS S3', 'AWS RDS (PostgreSQL)', 'AWS Cognito', 'AWS SES'
@@ -20,7 +31,33 @@ export const ProjectList: Array<ProjectModel> = [
     description:
       `Mobile application for assessing legal regulations, assigning work to appropriate departments, and
       monitoring legal compliance.`,
-    imagePath: [],
+    imagePath: [
+      'https://drive.google.com/uc?export=view&id=1HCKpt_mvRylbCnzLefgEW6xQ92eus32u',
+      'https://drive.google.com/uc?export=view&id=1nTFIpDi2RVSaPw2a9KY2xP3EtfHqplw1',
+      'https://drive.google.com/uc?export=view&id=16St4m74PLfZQDAfCGUY4CvR5RPOMlYpe',
+      'https://drive.google.com/uc?export=view&id=1PPwLIbWYIPEcE7wHWRiKhXC-p-eNyHWD',
+      'https://drive.google.com/uc?export=view&id=1XjJ1y2EMkmoa4iVLu0T7Td2w4bWurCgo',
+      'https://drive.google.com/uc?export=view&id=1q7mMHsw0II6Jtv_DpBZeQjKcnbFzZpHT',
+      'https://drive.google.com/uc?export=view&id=1Ux9CcDZhdXuzWyBWnCxvxOUjGiksv6Sp',
+      'https://drive.google.com/uc?export=view&id=1VfgfU5WLbVvGtQ_TBkY6vZZDPeJS4WAH',
+      'https://drive.google.com/uc?export=view&id=1Bg5GRi_dHAuOBQ8f9Dw_v1qC_Y3joySm',
+      'https://drive.google.com/uc?export=view&id=1UHTHF5FUf5PulPhRCFMrntU0VFure9vG',
+      'https://drive.google.com/uc?export=view&id=11z4zOsdc1LcR3XTVzKQ1Znj0kDr6QKVZ',
+      'https://drive.google.com/uc?export=view&id=1Nv8vY9fXqIoXDKTf-nmTjswD3P8ieaQe',
+      'https://drive.google.com/uc?export=view&id=1RLQsAdBv5mbJtVwZzFZdAWZllMpKiAfn',
+      'https://drive.google.com/uc?export=view&id=1oUxHBkOoWCZyW-hOLVQcHvGsPHUSu1N9',
+      'https://drive.google.com/uc?export=view&id=1mDOvx8m0t1ZXSEVxp7lYFtSnAs6__XD0',
+      'https://drive.google.com/uc?export=view&id=1FBAC1SorxQtaa2ASXmzdwgXN3Bt_xiRJ',
+      'https://drive.google.com/uc?export=view&id=165JX6JlRExMzenbHyZbH61wGlT4Fh30Q',
+      'https://drive.google.com/uc?export=view&id=1Zcesuf8lYe7LDlKjK7Xmr8LazNIa-0vW',
+      'https://drive.google.com/uc?export=view&id=1orF9eWViyKv9v1gxjiIPwwXnguBDOEmB',
+      'https://drive.google.com/uc?export=view&id=1UW-c1TiPEjsQ5GPeTVWsrmJSOuL2uHTF',
+      'https://drive.google.com/uc?export=view&id=16NS5DlLGbGcBC0a2976wtnl5GatPaQmN',
+      'https://drive.google.com/uc?export=view&id=1CQ_S-Nq4hthCAMIM1WjzOZZSnrzRaIAX',
+      'https://drive.google.com/uc?export=view&id=11pBgco1Cu5dOKwfvbsPZibGOtVDSfdy6',
+      'https://drive.google.com/uc?export=view&id=1ay5171or9jAzKISRLlinyyBmBzTHKZKr',
+      'https://drive.google.com/uc?export=view&id=137o3aucORsvv7rqlVbju4BtN5fkVzb3P',
+    ],
     skills: [
       'React Native', 'ASP.NET Core', 'C#', 'TypeScript', 'AWS RDS (PostgreSQL)'
     ],
@@ -30,7 +67,19 @@ export const ProjectList: Array<ProjectModel> = [
   {
     name: 'Time Attendance',
     description: `Web application for manager to manage employee's shift and OT plan and a dashboard for tracking amount of employee in their department.`,
-    imagePath: [],
+    imagePath: [
+      'https://drive.google.com/uc?export=view&id=1JwU_38ctZzkB1VsYcfzyP0ghE8meKG5h',
+      'https://drive.google.com/uc?export=view&id=1m-VbLX1IOlrxibwnHWD4LLxZjKF9Nw2C',
+      'https://drive.google.com/uc?export=view&id=1dyyDUikg-phA3iNKlzQJLvdJ8K_66cGu',
+      'https://drive.google.com/uc?export=view&id=1Hc__wBixwOIon_iFoMTLdZ7dI2Lt2wQH',
+      'https://drive.google.com/uc?export=view&id=1K6sxK1lJc8ITLDAtkAQTMPR9aydROgWu',
+      'https://drive.google.com/uc?export=view&id=1u4ZEC9ndnhJTOx2ps3otkRpCVC3QAfzV',
+      'https://drive.google.com/uc?export=view&id=1PZO-WLWCGK3B50aYkqb53eJ5YDeIJosF',
+      'https://drive.google.com/uc?export=view&id=1bUIOte_m7G8X8gw2obtmNcDUGM8yp4Fo',
+      'https://drive.google.com/uc?export=view&id=1l_SORZnYGWlS5VcVl_wVNaUoHiY1ELyP',
+      'https://drive.google.com/uc?export=view&id=1I-WvJ0rZDPGGL4TyN16JH0QZrj-7URgq',
+      'https://drive.google.com/uc?export=view&id=1-f_jXjXopNm1J3T5qRisE1ArRaX5evP7',
+    ],
     skills: [
       'Angular', 'Django REST', 'TypeScript', 'SCSS', 'Cypress', 'Cucumber'
     ],
@@ -40,7 +89,14 @@ export const ProjectList: Array<ProjectModel> = [
   {
     name: 'Rotten Potatoes',
     description: 'Web apllication for finding movie in TMDb with filtering, and rating movie by potatoes rate.',
-    imagePath: [],
+    imagePath: [
+      'https://drive.google.com/uc?export=view&id=12ZkhgitzGPqB_4N3lgPdCt0XpVoCtKZD',
+      'https://drive.google.com/uc?export=view&id=1QQn9tQCOkeOlEi4cm7H4v5fZGcksM27d',
+      'https://drive.google.com/uc?export=view&id=1EfCNDTDjC7FRo-up08A7WT-eWxOCASYg',
+      'https://drive.google.com/uc?export=view&id=184C6drJu0xo-yGBwomK16Y42eKqj7lN0',
+      'https://drive.google.com/uc?export=view&id=1-IkXvYhqeTceN12tlPmEdAIGFlGmOwNG',
+      'https://drive.google.com/uc?export=view&id=1FP2-TTx13U-XdiiMF5l8xELapaEULUl0',
+    ],
     skills: [
       'Rails', 'Ruby', 'Haml', 'HTML', 'JavaScript', 'SCSS', 'Cucumber', 'PostgreSQL'
     ],
@@ -50,7 +106,15 @@ export const ProjectList: Array<ProjectModel> = [
   {
     name: 'Taskky',
     description: 'Mobile application for organizing and planning on tasks.',
-    imagePath: [],
+    imagePath: [
+      'https://drive.google.com/uc?export=view&id=1WJ0pyDpRZhgfpOxVpY0I7mK4E3hnEcPm',
+      'https://drive.google.com/uc?export=view&id=1NWagxN5slfXdNBvGOjfMz4FRtjwp5xAu',
+      'https://drive.google.com/uc?export=view&id=1_jO1wbq7dg31cNVcwIuyQVTFkoIK1RzJ',
+      'https://drive.google.com/uc?export=view&id=1TAPxeOosGajXL99ehxKClNY_k1I9cc14',
+      'https://drive.google.com/uc?export=view&id=1mAH0GzpD_JBMZUdgfgB9bH_GFuD10Asz',
+      'https://drive.google.com/uc?export=view&id=1bcpSoUpzkEzl5dNFOZs8Dp8FaP9q6EW9',
+      'https://drive.google.com/uc?export=view&id=1pYhrY4gbAV21Qu464d2hTD2EMEA9MnI-',
+    ],
     skills: [
       'NativeScipt', 'Angular', 'TypeScript', 'HTML', 'CSS', 'JavaScript'
     ],
@@ -62,7 +126,10 @@ export const ProjectList: Array<ProjectModel> = [
     description:
       `Web application for tracking people who enter laboratories.
       COVID-19 tracker for ECE department at KMUTNB and `,
-    imagePath: [],
+    imagePath: [
+      'https://drive.google.com/uc?export=view&id=1L7gtojSxJKHygweLvs0MCpWdFUvFx6xa',
+      'https://drive.google.com/uc?export=view&id=1Z879pkOiZofV1D0VdidBI2j-EgVmrMdp'
+    ],
     skills: [
       'Django', 'Python', 'JavaScript', 'jQuery', 'HTML', 'CSS'
     ],
