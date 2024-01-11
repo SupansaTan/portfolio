0ad4c4cefd61c4f4c0fdbed0bbd39db6ce5c0533
Supansa Tantulset <supansa@sirisoft.co.th>
2024-01-11 10:26:17 +0700

    update experience and about me

--- src/app/constants/about-me.constant.ts
+++ src/app/constants/about-me.constant.ts
@@ -1,7 +1,7 @@
 export const AboutMe: string =
-`Now, I have just completed my Bachelor’s degree in Computer Engineering at King Mongkut University of Technology North Bangkok (KMUTNB).
+`Now, I have completed my Bachelor’s degree in Computer Engineering at King Mongkut University of Technology North Bangkok (KMUTNB).
 
-I have had the opportunity to intern as a Full Stack Developer for 3 months, working mainly with Angular and ASP.NET Core frameworks.
+Experience in Software Developer for 5 months with support team about deployment, frontend technical issues, and unit testing. Intern as a Full Stack Developer for 3 months, working mainly with Angular and ASP.NET Core frameworks.
 
-I also open to doing freelance work during summer semester of second-year, primarily creating websites from designs. therefore, I usually work with React, Angular, HTML, CSS, SCSS, and JavaScript.
+I also open to doing freelance work during summer semester of second-year, primarily creating responsive websites from designs, and usually work with React, Angular, HTML, CSS, SCSS, and JavaScript.
 `
--- src/app/constants/experience.constant.ts
+++ src/app/constants/experience.constant.ts
@@ -2,6 +2,24 @@ import { ExperienceModel } from "../models/experience.model";
 
 export const ExperienceList: Array<ExperienceModel> = [
   {
+    role: 'Software Developer',
+    company: 'Sirisoft Public Company Limited',
+    startDate: 'August 2023',
+    endDate: 'Present',
+    description:
+      `- Setup a Jenkins pipeline for Angular linting (ESLint), unit testing (Jest), and end-to-end testing.
+      - Configure ESLint, Jest, and Cypress with Cucumber ni frontend repositories.
+      - Enhance the Jenkins pipeline to minimize dependency installation time and address issues causing pipeline failures.
+      - Setup Gitlab webhooks to trigger the Jenkins pipeline build on push event.
+      - Provide assistance to the team regarding deployment, unit testing, end-to-end testing, and frontend technical issues.
+      - Conduct a knowledge-sharing session on writing unit tests using Jest and version tagging ni projects.
+      - Lead of the frontend team and restructured frontend code for increased generality.
+      - Implement modules for managing user payments, officer management, and registration.`,
+    skill: [
+      'Angular', 'TypeScript', 'Jenkins', 'Docker', 'Kubernetes', 'NestJS'
+    ]
+  },
+  {
     role: 'Software Developer Internship',
     company: 'BUILK ONE GROUP',
     startDate: 'June 2022',
