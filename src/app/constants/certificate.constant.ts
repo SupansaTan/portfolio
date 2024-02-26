22062011f3708736d9af6ee6c8ae1e9ba071ea5c
Supansa Tantulset <supansa.tantulset@gmail.com>
2024-02-27 02:41:13 +0700

    fix image url not working

--- src/app/constants/certificate.constant.ts
+++ src/app/constants/certificate.constant.ts
@@ -6,13 +6,13 @@ export const CertificateList: Array<CertificateModel> = [
     description: 'Participated in the Codegoda 2023',
     organize: 'Agoda',
     year: '2023',
-    image: 'https://drive.google.com/uc?export=view&id=1EYvcONeDle2PGy7sWX6rWFZZ1ndvbXNc'
+    image: 'https://drive.google.com/thumbnail?id=1EYvcONeDle2PGy7sWX6rWFZZ1ndvbXNc&sz=w1000'
   },
   {
     title: 'Certificate of Completion',
     description: 'Completing the "Young software developer program"',
     organize: 'CPF',
     year: '2021',
-    image: 'https://drive.google.com/uc?export=view&id=1uer1sfI-0WFmx1-N7yhLkcCN9Embe9z0'
+    image: 'https://drive.google.com/thumbnail?id=1uer1sfI-0WFmx1-N7yhLkcCN9Embe9z0&sz=w1000'
   },
 ]
